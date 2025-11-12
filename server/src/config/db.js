40cf61805a5bb9ed81d30e66364b28dca6fbfab8
Youssef Effat <141458994+KEMONOYA@users.noreply.github.com>
2025-11-12 15:17:58 +0200

    added tests

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
 
+dotenv.config();
 export async function connectDB() {
   const uri = process.env.MONGO_URI;
   if (!uri) throw new Error('MONGO_URI is not set');
